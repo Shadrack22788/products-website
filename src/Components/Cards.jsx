112d33122e45ab36731bd1b9913f1ddbd5787624
Shadrack22788 <tuyizeresh@gmail.com>
2025-12-04 13:34:49 +0200

    Add responsive design

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -8,7 +8,7 @@ const Cards = ({ title, description, image }) => {
             p-6">
                 <h6 className="text-lg font-bold">{title} </h6>
                 <p className="text-lg  py-6">{description}</p>
-                <img src={image} className="rounded md:rounded-3xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-4 w-54 h-1/3 " alt="image" />
+                <img src={image} className="rounded md:rounded-3xl  gap-4 w-54 h-1/3 " alt="image" />
             </div>
         </div>
 
